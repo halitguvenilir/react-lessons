55c67b3e51d3894ed1f4efb78333fd5598402895
Halit <halitguvenilir@gmail.com>
2021-01-03 00:53:10 +0300

    Initialized commit

---
+++ pages/hooks-1.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
